75640d03ebc359d7d0b8b3eda7621823d2a463b1
Ankit Kumar <120427738+blackhat-coder21@users.noreply.github.com>
2023-10-01 19:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ if uploaded_file is not None:
     df = preprocessor.preprocess(data)
 
     # fetch unique users
-    user_list = df['user'].unique().tolist()
+    # user_list = df['user'].unique().tolist()
     # user_list.remove('group_notification')
-    user_list.sort()
-    user_list.insert(0,"Overall")
+    # user_list.sort()
+    # user_list.insert(0,"Overall")
 
     selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
 
